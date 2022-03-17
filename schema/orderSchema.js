2e63d7b8a62fa17885f2061ae3b5fe1b64e74a08
msmaxiii <maxwellrg@gmail.com>
2022-03-17 11:11:07 -0400

    added book

--- schema/orderSchema.js
+++ schema/orderSchema.js
@@ -4,7 +4,8 @@ const orderSchema = new mongoose.Schema({
     order_id:{type:String, required:true},
     name:{type:String, required:true},
     address:{type:String, required:true},
-    email:{type:String, required:true}
+    email:{type:String, required:true},
+    password:{type:String,required:true}
 
 })
 module.exports = mongoose.model('order',orderSchema)
